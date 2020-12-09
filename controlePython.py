d52139b8c4ab4b8bfde98273a827cf18667fb248
MichenaudMelvin <74654895+MichenaudMelvin@users.noreply.github.com>
2020-12-09 10:14:38 +0100

    Update controlePython.py

--- controlePython.py
+++ controlePython.py
@@ -6,18 +6,21 @@ import random
 #fonctions
 def tourJoueur(motMystere):
 	motJoueur = input("Entrez un mot : ")
-	print(motJoueur)
+	print(motMystere)
 	if len(motJoueur) > 6:
 		print("Entrez un mot de 6 lettres")
 	else:
 		for i in range(0,6):
 			if motJoueur[i] == motMystere[i]:
 				print(Back.RED + motJoueur[i], end="")
+			else:
+				print(Back.BLUE + motJoueur[i], end="")
 
 #main program
-listeDeMot = ["castor","cinema","citron","cypres","basile","totoro","atouts","romain","projet","macron"]
+listeDeMot = ["castor","cinema","citron","cypres","basile","totoro","atouts","romain","bateau","macron"]
 motAleatoire = random.randint(0,9)
 motMystere = listeDeMot[motAleatoire]
+tour = 0
 print("motMystere = ", motMystere)
 
 tourJoueur(motMystere)
